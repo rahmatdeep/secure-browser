b236c8313d72cd3d796a28a0f8c896d03ecc541a
rahmatdeep <rahmatdeep@gmail.com>
2025-08-21 01:30:14 +0530

    more ports

--- backend/src/utils/portPool.ts
+++ backend/src/utils/portPool.ts
@@ -5,11 +5,18 @@ const portPool: Record<
   number,
   { subdomain: string; status: "free" | "occupied" }
 > = {
-  25100: { subdomain: "vnc1.secure-browser.rahmatdeep.com", status: "free" },
-  25101: { subdomain: "vnc2.secure-browser.rahmatdeep.com", status: "free" },
-  25102: { subdomain: "vnc3.secure-browser.rahmatdeep.com", status: "free" },
-  25103: { subdomain: "vnc4.secure-browser.rahmatdeep.com", status: "free" },
-  25104: { subdomain: "vnc5.secure-browser.rahmatdeep.com", status: "free" },
+  25101: { subdomain: "vnc1.secure-browser.rahmatdeep.com", status: "free" },
+  25102: { subdomain: "vnc2.secure-browser.rahmatdeep.com", status: "free" },
+  25103: { subdomain: "vnc3.secure-browser.rahmatdeep.com", status: "free" },
+  25104: { subdomain: "vnc4.secure-browser.rahmatdeep.com", status: "free" },
+  25105: { subdomain: "vnc5.secure-browser.rahmatdeep.com", status: "free" },
+  25106: { subdomain: "vnc6.secure-browser.rahmatdeep.com", status: "free" },
+  25107: { subdomain: "vnc7.secure-browser.rahmatdeep.com", status: "free" },
+  25108: { subdomain: "vnc8.secure-browser.rahmatdeep.com", status: "free" },
+  25109: { subdomain: "vnc9.secure-browser.rahmatdeep.com", status: "free" },
+  25110: { subdomain: "vnc10.secure-browser.rahmatdeep.com", status: "free" },
+  25111: { subdomain: "vnc11.secure-browser.rahmatdeep.com", status: "free" },
+  25112: { subdomain: "vnc12.secure-browser.rahmatdeep.com", status: "free" },
 };
 
 // Function to get a free port
